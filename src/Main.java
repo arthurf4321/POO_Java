73cdb6c1953e59e65704501240fc12cc2a4355ea
Arthurf <arthurf43213@gmail.com>
2025-08-11 08:46:28 -0300

    Exercicio 3: POO Java

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,63 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        MaquinaBanho maquina = new MaquinaBanho();
 
-        ContaBancaria conta = new ContaBancaria(400f);
+        while (true) {
+            System.out.println("\n=== Controle da Máquina de Banho ===");
+            System.out.println("1. Colocar pet na máquina");
+            System.out.println("2. Dar banho no pet");
+            System.out.println("3. Retirar pet da máquina");
+            System.out.println("4. Limpar máquina");
+            System.out.println("5. Abastecer água");
+            System.out.println("6. Abastecer shampoo");
+            System.out.println("7. Verificar nível de água");
+            System.out.println("8. Verificar nível de shampoo");
+            System.out.println("9. Mostrar status completo");
+            System.out.println("0. Sair");
+            System.out.print("Escolha uma opção: ");
 
-        conta.consultarSaldo();
-        conta.consultarChequeEspecial();
+            int opcao = scanner.nextInt();
 
-        conta.sacar(450f);
-        conta.consultarSaldo();
-
-        conta.depositar(100f);
-        conta.consultarSaldo();
-
-        conta.pagarBoleto(30f);
-        conta.consultarSaldo();
-
-        System.out.println("Usando cheque especial? " + conta.usandoChequeEspecial());
-
-        scanner.close();
+            switch (opcao) {
+                case 1:
+                    maquina.colocarPet();
+                    break;
+                case 2:
+                    maquina.darBanho();
+                    break;
+                case 3:
+                    maquina.retirarPet();
+                    break;
+                case 4:
+                    maquina.limparMaquina();
+                    break;
+                case 5:
+                    maquina.abastecerAgua();
+                    break;
+                case 6:
+                    maquina.abastecerShampoo();
+                    break;
+                case 7:
+                    maquina.verificarNivelAgua();
+                    break;
+                case 8:
+                    maquina.verificarNivelShampoo();
+                    break;
+                case 9:
+                    maquina.statusMaquina();
+                    break;
+                case 0:
+                    System.out.println("Saindo do sistema...");
+                    scanner.close();
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opção inválida! Tente novamente.");
+            }
+        }
     }
 }
---
+++ src/MaquinaBanho.java
@@ -0,0 +1,130 @@
+import java.util.Scanner;
+
+public class MaquinaBanho {
+
+    //capacidade maxima
+    private final int MAX_AGUA = 30;
+    private final int MAX_SHAMPOO = 10;
+
+    //consumo por operacao
+    private final int CONSUMO_AGUA_BANHO = 10;
+    private final int CONSUMO_SHAMPOO_BANHO = 2;
+
+    private final int CONSUMO_AGUA_LIMPEZA = 3;
+    private final int CONSUMO_SHAMPOO_LIMPEZA = 1;
+
+    //abastecimento por vez
+    private final int ABASTECIMENTO_AGUA = 2;
+    private final int ABASTECIMENTO_SHAMPOO = 2;
+
+    private int agua;
+    private int shampoo;
+    private boolean temPet = false;
+    private boolean petLimpo = false;
+    private boolean maquinaSuja = false;
+
+
+    public MaquinaBanho() {
+        this.agua = 0;
+        this.shampoo = 0;
+    }
+
+    public void colocarPet() {
+        if(!temPet && !maquinaSuja) {
+            temPet = true;
+        }else{
+            System.out.println("A maquina ja esta sendo usada ou esta suja!!!");
+        }
+    }
+
+    public void darBanho() {
+        if(temPet) {
+            if(agua >= 10 && shampoo >= 2) {
+                agua -= 10;
+                shampoo -= 2;
+                petLimpo = true;
+                System.out.println("Seu pet esta limpo!!!");
+            }else{
+                System.out.println("A maquina precisa ser reabastecida!!!");
+            }
+        }else{
+            System.out.println("Coloque o pet na maquina!!!");
+        }
+    }
+
+    public void retirarPet() {
+        if (temPet) {
+            if (!petLimpo) {
+                maquinaSuja = true;
+                System.out.println("Pet retirado sujo, a máquina precisa de limpeza!");
+            } else {
+                System.out.println("Pet retirado limpo, máquina pronta para outro banho.");
+            }
+            temPet = false;
+            petLimpo = false;
+        } else {
+            System.out.println("Não há pet na máquina para retirar!");
+        }
+    }
+
+
+    public void limparMaquina() {
+        if(maquinaSuja) {
+            if(agua >= CONSUMO_AGUA_LIMPEZA && shampoo >= CONSUMO_SHAMPOO_LIMPEZA) {
+                agua -= CONSUMO_AGUA_LIMPEZA;
+                shampoo -= CONSUMO_SHAMPOO_LIMPEZA;
+                maquinaSuja = false;
+                System.out.println("Limpeza completa");
+            }else{
+                System.out.println("Voce precisa reabastecer a maquina");
+            }
+        }else{
+            System.out.println("Sua maquina ja esta limpa");
+        }
+    }
+
+    public void abastecerAgua() {
+        int valor = ABASTECIMENTO_AGUA;
+        if(agua != MAX_AGUA) {
+            if(agua + valor <= MAX_AGUA) {
+                agua = agua + valor;
+                System.out.println("Voce reabasteceu a maquina com Agua");
+            }else{
+                System.out.println("A maquina nao suporta essa quantidade de agua!!!");
+            }
+        }else{
+            System.out.println("A maquina ja esta com a capacidade maxima de Agua");
+        }
+    }
+
+    public void abastecerShampoo() {
+        int valor = ABASTECIMENTO_SHAMPOO;
+        if(shampoo != MAX_SHAMPOO) {
+            if(shampoo + valor <= MAX_SHAMPOO) {
+                shampoo = shampoo + valor;
+                System.out.println("Voce reabasteceu a maquina com Shampoo");
+            }else{
+                System.out.println("A maquina nao suporta essa quantidade de shampoo!!!");
+            }
+        }else{
+            System.out.println("A maquina ja esta com a capacidade maxima de Shampoo");
+        }
+    }
+
+    public void verificarNivelAgua() {
+        System.out.println("A máquina tem: " + agua + (agua == 1 ? " litro" : " litros") + " de água");
+    }
+
+    public void verificarNivelShampoo() {
+        System.out.println("A máquina tem: " + shampoo + (shampoo == 1 ? " litro" : " litros") + " de shampoo");
+    }
+
+    public void statusMaquina() {
+        System.out.println("\n=== Status da Máquina ===");
+        System.out.println("Água: " + agua + "/" + MAX_AGUA + " litros");
+        System.out.println("Shampoo: " + shampoo + "/" + MAX_SHAMPOO + " litros");
+        System.out.println("Tem pet na máquina: " + (temPet ? "Sim" : "Não"));
+        System.out.println("Pet limpo: " + (petLimpo ? "Sim" : "Não"));
+        System.out.println("Máquina suja: " + (maquinaSuja ? "Sim" : "Não"));
+    }
+}
