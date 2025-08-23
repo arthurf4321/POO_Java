d47d995fd9dbc3c4525f5aae23ca9c8dc8030ef9
Arthurf <arthurf43213@gmail.com>
2025-08-23 19:28:10 -0300

    Exercicio 9 POO Basic

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,13 @@ package POO_Basic;
         pessoa1.idade = 12;
         pessoa1.falar();
 
+
         CarroInfos carro1 = new CarroInfos();
         carro1.modelo = "Civic";
         carro1.ano = 2007;
         carro1.marca = "Honda";
         carro1.exibirInfos();
+
 
         Cachorro cachorro1 = new Cachorro();
         cachorro1.raca = "PitBul";
@@ -24,11 +26,13 @@ package POO_Basic;
         cachorro1.exameIdade();
         cachorro1.exameRaca();
 
+
         Livro livro1 = new Livro();
         livro1.autor = "Arthur";
         livro1.titulo = "O mundo por tras da sociedade";
         livro1.ano = 1930;
         livro1.exibirInfos();
+
 
         Celular celular1 = new Celular();
         celular1.marca = "Android";
@@ -37,6 +41,12 @@ package POO_Basic;
 
         celular1.ligar();
         celular1.desligar();
+
+
+        Canetta caneta1 = new Canetta();
+        caneta1.cor = "Azul";
+        caneta1.ponta = 0.5;
+        caneta1.escrever();
      */
         
     }
---
+++ src/POO_Basic/Canetta.java
@@ -0,0 +1,10 @@
+package POO_Basic;
+
+public class Canetta {
+    public String cor;
+    public double ponta;
+
+    public void escrever() {
+        System.out.println("Escrevendo com a: " + ponta + " e cor: " + cor);
+    }
+}
